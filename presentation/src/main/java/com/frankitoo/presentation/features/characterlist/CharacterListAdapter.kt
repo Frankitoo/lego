6ecafd3f906e1ecfabdcd211855f3512a5ea1262
Frankito <fenyov.boldi@gmail.com>
2020-12-03 19:19:03 +0100

    add list animation

--- presentation/src/main/java/com/frankitoo/presentation/features/characterlist/CharacterListAdapter.kt
+++ presentation/src/main/java/com/frankitoo/presentation/features/characterlist/CharacterListAdapter.kt
@@ -3,6 +3,8 @@ package com.frankitoo.presentation.features.characterlist
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.paging.PagingDataAdapter
@@ -24,6 +26,9 @@ class CharacterListAdapter :
         CharacterDiffUtilCallback()
     ) {
     var onClickListener: ((Character) -> Unit)? = null
+
+    // Allows to remember the last item shown on screen
+    private var lastPosition = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CharacterViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_character, parent, false)
@@ -50,6 +55,16 @@ class CharacterListAdapter :
         }
 
         getItem(position)?.let { holder.bindView(it) }
+
+        setAnimation(holder.itemView, position)
+    }
+
+    private fun setAnimation(viewToAnimate: View, position: Int) {
+        if (position > lastPosition) {
+            val animation: Animation = AnimationUtils.loadAnimation(viewToAnimate.context, R.anim.slide_from_bottom)
+            viewToAnimate.startAnimation(animation)
+            lastPosition = position
+        }
     }
 
     class CharacterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
